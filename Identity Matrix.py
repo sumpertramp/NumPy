761324ef50f3d15cf1a846c8d6d82c8f25e19f6f
Sümeyye Derelli <90193242+sumpertramp@users.noreply.github.com>
2022-08-03 10:36:18 +0300

    Add files via upload

---
+++ Identity Matrix.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+#Identity (Birim) Matrix Oluşturmak
+#Birim matrix çarpma işleminde etkisiz matrixtir
+
+print(np.eye(3,4)) #3 sütun 4 satırlık birim matrix oluşturuldu
+
+print(np.eye(3,3))
